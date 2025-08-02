c34c9393375e6415ee45f18fd39f4e1da6cd3d79
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-08-02 22:02:43 +0800

    fix: inherit path variable

--- live.sh
+++ live.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+export PATH=$PATH:$HOME/.cargo/bin
+
 if systemctl --user is-active --quiet mdbook-live; then
     systemctl --user stop mdbook-live
 fi
@@ -9,4 +11,4 @@ if systemctl --user is-failed --quiet mdbook-live; then
     systemctl --user reset-failed mdbook-live
 fi
 
-systemd-run --user --unit=mdbook-live --working-directory=. mdbook serve
+systemd-run --user --unit=mdbook-live --working-directory=. --setenv=PATH="$PATH" mdbook serve
