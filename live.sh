0edadb44d7726580661a0608ecd4a17dd727e5e3
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-08-02 11:25:31 +0800

    add script for live preview

---
+++ live.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+if systemctl --user is-active --quiet mdbook-live; then
+    systemctl --user stop mdbook-live
+fi
+
+if systemctl --user is-failed --quiet mdbook-live; then
+    systemctl --user reset-failed mdbook-live
+fi
+
+systemd-run --user --unit=mdbook-live --working-directory=. mdbook serve
